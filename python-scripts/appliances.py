a38366e87a4d7503cfbc1f8b4e3da455dcf4c3fe
ahmedyar7 <ahmedyar778@gmail.com>
2024-05-12 12:22:15 +0500

    DATABASE PROJECET

--- python-scripts/appliances.py
+++ python-scripts/appliances.py
@@ -1,7 +1,23 @@
 from bs4 import BeautifulSoup
 from requests import get
-import pandas as pd
 
-file = pd.read_html("https://telemart.pk/home-and-kitchen-appliances.html")
-file_df = pd.DataFrame(file)
-print(file_df)
+
+class ScrapeAppliances:
+    def __init__(self) -> None:
+        self.response = get(url="https://telemart.pk/home-and-kitchen-appliances.html")
+        self.data = self.response.text
+
+        self.soup = BeautifulSoup(markup=self.data, features="html.parser")
+
+    def get_data(self):
+        """This function would get the data"""
+
+        self.item = self.soup.find(
+            "div h4",
+            class_="col-span-3.bg-white.relative.cursor-pointer.p-0.5",
+        )
+        print(self.item)
+
+
+scrape = ScrapeAppliances()
+scrape.get_data()
